f7d77f59552af602e97cb58ce16614143c25a25a
admin <admin@envosha.com>
2017-04-27 19:55:21 +0800

    Changes related to pythion docx

--- reporting/urls.py
+++ reporting/urls.py
@@ -5,8 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^cem/$', views.invoice_view, name='invoice_view'),
-    url(r'^pd/$', views.write_pdf_view, name='write_pdf_view'),
-    url(r'^pdo/$', views.html_to_pdf_view, name='html_to_pdf_view'),
 
 
 ]
