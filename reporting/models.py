b366eb1621c5e57dbeae07767b623d6cc0f3bd46
admin <admin@envosha.com>
2017-04-28 17:56:50 +0800

    Changes related to python docx

--- reporting/models.py
+++ reporting/models.py
@@ -1,7 +1,5 @@
 from __future__ import unicode_literals
 
-from django.db import models
-
 # Create your models here.
 from django.db import models
 
@@ -12,3 +10,31 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Company(models.Model):
+    company_name = models.CharField(max_length=200)
+    company_address = models.CharField(max_length=200)
+    company_contact_no = models.CharField(max_length=200)
+    pic_name = models.CharField(max_length=200)
+    consultant = models.CharField(max_length=200)
+
+
+class Type(models.Model):
+    company_name = models.CharField(max_length=200)
+    point = models.CharField(max_length=200)
+    work_unit = models.CharField(max_length=200)
+    method = models.CharField(max_length=200)
+    flow_rate = models.CharField(max_length=200)
+    media = models.CharField(max_length=200)
+    technique = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+    classification = models.CharField(max_length=200)
+    classification_value = models.CharField(max_length=200)
+
+
+class Parameter(models.Model):
+    company_name = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+    point = models.CharField(max_length=200)
+    parameter_value = models.CharField(max_length=200)
